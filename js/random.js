ff55a5f606fb3bd113f836a8131245cb14abca94
Curt Hill <nwres1@yahoo.com>
2016-05-13 18:02:25 -0700

    added random quote javascript

--- js/random.js
+++ js/random.js
@@ -1,5 +1,15 @@
 var random = (function() {
 
+    var quotes = [
+        "quote a",
+        "quote b",
+        "quote c",
+        "quote d",
+        "quote e",
+        "quote f",
+    ];
+
+
     var init = function () {
         $(function() {
             showQuote();
@@ -10,8 +20,23 @@ var random = (function() {
     };
 
     function showQuote() {
+        $("#divQuote").text(getQuote());
         $("#divQuote").fadeIn("slow");
     }
+
+    function getQuote() {
+        var numberOfQuotes = quotes.length;
+        var randomQuoteIndex = getRandomNumber(0, numberOfQuotes - 1);
+        return quotes[randomQuoteIndex];
+
+    }
+
+    function getRandomNumber(minVal, maxVal) {
+        var numberOfPossibleValues = (maxVal + 1) - minVal;
+        return Math.floor(Math.random() * numberOfPossibleValues) + minVal;
+    }
+
+
     return {
         init: init
     };
